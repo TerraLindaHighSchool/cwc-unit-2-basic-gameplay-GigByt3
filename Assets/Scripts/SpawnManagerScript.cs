61d3f7154c09c2371e1067547f5627be4ea4c269
Finlay Norton-Lindsay <p3n.duke@gmail.com>
2020-01-27 21:39:53 -0800

    Lesson 2.4

--- Assets/Scripts/SpawnManagerScript.cs
+++ Assets/Scripts/SpawnManagerScript.cs
@@ -7,15 +7,20 @@ public class SpawnManagerScript : MonoBehaviour
     public GameObject[] animals;
     public int spawnPosZ;
     public int spawnRange;
+    public float startDelay;
+    public float spawnInterval;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-        	int animalIndex = Random.Range(0, animals.Length);
-        	Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
-        	Instantiate(animals[animalIndex], spawnPos, animals[animalIndex].transform.rotation);
-        }
+        InvokeRepeating("SpawnRangomAnimal", startDelay, spawnInterval);
+    }
+
+    void SpawnRangomAnimal()
+    {
+    	int animalIndex = Random.Range(0, animals.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
+        Instantiate(animals[animalIndex], spawnPos, animals[animalIndex].transform.rotation);
+
     }
 }
