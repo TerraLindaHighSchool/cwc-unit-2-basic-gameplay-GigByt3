220e55ea5e69c17a46d7f06d70eb8bad17eab5a1
Finlay Norton-Lindsay <p3n.duke@gmail.com>
2020-01-27 20:55:38 -0800

    Lesson 2.3

---
+++ Assets/Scripts/SpawnManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerScript : MonoBehaviour
+{
+    public GameObject[] animals;
+    public int spawnPosZ;
+    public int spawnRange;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+        	int animalIndex = Random.Range(0, animals.Length);
+        	Vector3 spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, spawnPosZ);
+        	Instantiate(animals[animalIndex], spawnPos, animals[animalIndex].transform.rotation);
+        }
+    }
+}
